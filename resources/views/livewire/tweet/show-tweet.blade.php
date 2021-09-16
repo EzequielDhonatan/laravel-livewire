25ab17b3881379108678a05032ddf01d0159a0de
Ezequiel Dhonatan <Contato@ezequieldhonatan.com.br>
2021-09-16 15:28:24 -0300

    Action - Like and Unlike

--- resources/views/livewire/tweet/show-tweet.blade.php
+++ resources/views/livewire/tweet/show-tweet.blade.php
@@ -23,11 +23,11 @@
 
         @if ( $tweet->likes->count() )
 
-            <a href="">Descurtir</a>
+            <a href="#" wire:click.prevent="unlike( {{ $tweet->id }} )">Descurtir</a>
 
         @else
 
-            <a href="">Curtir</a>
+            <a href="#" wire:click.prevent="like( {{ $tweet->id }} )">Curtir</a>
 
         @endif
 
